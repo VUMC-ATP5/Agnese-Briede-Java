31987307a4b1bd5e480fc1f627589ec12e1c1016
abriede <briedeagnes@gmail.com>
2022-10-10 18:04:33 +0300

    Java 1 lekcijas majas darbs

--- src/main/java/lekcijaViens/majasDarbs/Main.java
+++ src/main/java/lekcijaViens/majasDarbs/Main.java
@@ -1,8 +1,28 @@
 package lekcijaViens.majasDarbs;
 
-
 public class Main {
     public static void main(String[] args) {
+
+        String countryName = "Sri Lanka";
+        int population= 22156000;
+        double area = 65610;
+        String capital = "Colombo";
+        String language1 = "Sinhala";
+        String language2 = "Tamil";
+        boolean isPartOfEU =false;
+        char currency = 'R';
+
+        System.out.println( countryName + " is an island country in South Asia." );
+        System.out.println("Sri Lanka has a population of around 22 million, to be precise: "+ population +".");
+        System.out.println("I have a friend who is from " + capital+" and he speaks mostly " + language1 + " and a little bit of " + language2 + ".");
+        System.out.println("Since " + countryName + "is in Asia, its not part of EU.");
+        System.out.println("In " +countryName+ " they have ruupies and somethimes its defined as " + currency + "however usually it is LKR.");
+
+
+
+
+
+
 
 
     }
